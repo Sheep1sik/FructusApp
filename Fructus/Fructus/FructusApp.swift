e1d62e41e3ee94809d3732aa58740188ca746ab5
Sheep1sik <bohg6799@naver.com>
2024-02-06 17:39:11 +0900

    Feat: Setup Project

---
+++ Fructus/Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by 양원식 on 2024/02/06.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
